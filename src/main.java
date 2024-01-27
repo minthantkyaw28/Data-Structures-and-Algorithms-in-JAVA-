5b275184074baabfe87895ec3597cf1dafe2393f
minthantkyaw28 <wanderhobby@gmail.com>
2024-01-27 08:14:21 +0630

    Test about Queue Data Structure and operations. And Explore about types of Data Structures.

--- src/main.java
+++ src/main.java
@@ -1,26 +1,51 @@
 import data_structures.Stack;
 
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class main {
     public static void main(String[] args) {
 
-        Stack stack = new Stack(5);
+//        Stack Data Type
+//        Stack stack = new Stack(5);
+//
+//        stack.push("Java");
+//        stack.push("Python");
+//        stack.push("C#");
+//        stack.push("C");
+//        stack.push("C++");
+//
+//        stack.pop();
+//
+//        stack.push("Javascript");
+//
+//        stack.printAll();
+//
+//        stack.peek();
+//
+//        System.out.println("Is Empty? " + stack.isEmpty());
+//        System.out.println("Is Full? " + stack.isFull());
+//        System.out.println("Size: " + stack.size());
 
-        stack.push("Java");
-        stack.push("Python");
-        stack.push("C#");
-        stack.push("C");
-        stack.push("C++");
+//        Queue Data Type
+          Queue<String> queue= new LinkedList<String>();
 
-        stack.pop();
 
-        stack.push("Javascript");
+          queue.add("A");
+        queue.add("B");
+        queue.add("C");
 
-        stack.printAll();
+        System.out.println(queue);
 
-        stack.peek();
+        queue.poll();
 
-        System.out.println("Is Empty? " + stack.isEmpty());
-        System.out.println("Is Full? " + stack.isFull());
-        System.out.println("Size: " + stack.size());
+        System.out.println(queue);
+
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.size());
+        System.out.println(queue.contains("B"));
+
     }
 }
